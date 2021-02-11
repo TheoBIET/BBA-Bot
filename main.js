abcc7fcd61a9b6ed428f3324cb65134f8b261c9c
Théo <dev.theobiet@gmail.com>
2021-02-11 20:33:12 +0100

    information updated

--- main.js
+++ main.js
@@ -84,5 +84,13 @@ client.on('message', message => {
 
 });
 
-client.on('ready', () => { client.user.setUsername('BBA').catch(console.error); console.log(`${client.user.tag} connecté.`); });
+client.on('ready', () => { 
+    // Mise à jour rapides des informations du BOT
+    //client.user.setUsername('蛇喰 夢子').catch(console.error); 
+    console.log(`${client.user.tag} connecté.`); 
+    //client.user.setAvatar('https://cdn.discordapp.com/attachments/644569508337549324/809504010649862155/1016983c9d7552e6.png');
+    client.user.setActivity("ƊɑѵƊɑѵ me coder", {
+        type: "WATCHING",
+      });
+});
 client.login(TOKEN);
